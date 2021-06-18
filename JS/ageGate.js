f5d65556e3a53df9217271bdf16005942abfaee5
DesireeFredenburg <des.fredenburg@gmail.com>
2021-06-18 10:23:51 -0700

    pushing age gate challenge

--- JS/ageGate.js
+++ JS/ageGate.js
@@ -1,3 +1,23 @@
+//This function calls the age gate. 
+function ageGate() {
+    document.getElementById("myAgeGate").style.display = "display";
+}
+
+//remember me check box function
+function checkBox() {
+    // Get the checkbox
+    var checkBox = document.getElementById("myCheck");
+    // Get the output text
+    var text = document.getElementById("check");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+
 //These functions open and close the contact from
 function openForm() {
     document.getElementById("myForm").style.display = "block";
